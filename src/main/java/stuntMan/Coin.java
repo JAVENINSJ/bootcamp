fb967696d4fd42d722bfe0925160eb07b989c4cb
Pukitis <rovsvg@gmail.com>
2021-08-07 19:43:05 +0300

    Coin animation and background brute force fix

--- src/main/java/stuntMan/Coin.java
+++ src/main/java/stuntMan/Coin.java
@@ -15,8 +15,10 @@ public class Coin extends JLabel {
 	static int frameWidth = GameScreen.frameWidth, frameHeight = GameScreen.frameHeight;
 	String name;
 	int x, y;
+	public static int animationFrame;
 	double speedX = 0, speedY = 0, distanceX, distanceY;
 	Rectangle hitbox;
+	JLabel coin;
 
 	public Coin(int x, int y, int width, int height, int rangeX, int rangeY, double posX, double posY, String pngName) {
 		super(new ImageIcon(new ImageIcon(JavaLabel.fRoute + "//GameAssets//" + pngName + ".png").getImage()
@@ -30,22 +32,29 @@ public class Coin extends JLabel {
 		this.setBounds(x, y, width, height);
 		this.hitbox = this.getBounds();
 		GameScreen.layers.get("gameLayer").add(this, (Integer) 1);
-		if ("Coin".equals(pngName)) {
+		if ("CoinBase".equals(pngName)) {
+			coin = new JLabel(new ImageIcon(new ImageIcon(JavaLabel.fRoute + "//GameAssets//Coin.png").getImage()
+					.getScaledInstance(width, height * 4, Image.SCALE_SMOOTH)), JLabel.CENTER);
+			coin.setBounds(0, 0, width, height * 4);
+			this.add(coin);
 			GameScreen.coins.add(this);
 		}
-		System.out.println(this.getLocation());
 	}
 
 	public boolean checkForDespawn(double distanceX, double distanceY) {
 		this.setPosition(distanceX, distanceY);
-		if (this.getX() < -2 * frameWidth || this.getX() > 4 * frameWidth
-				|| this.getY() < -2 * frameHeight || this.getY() > 4 * frameHeight) {
+		if (this.getX() < -2 * frameWidth || this.getX() > 4 * frameWidth || this.getY() < -2 * frameHeight
+				|| this.getY() > 4 * frameHeight) {
 			GameScreen.layers.get("gameLayer").remove(this);
 			return true;
 		}
 		return false;
 	}
-	
+
+	public void animate() {
+		this.coin.setLocation(0, (-animationFrame / 10) * this.getHeight());
+	}
+
 	public void setPosition(double moveX, double moveY) {
 		this.setLocation(x + (int) (moveX - distanceX), y + (int) (moveY - distanceY));
 	}
