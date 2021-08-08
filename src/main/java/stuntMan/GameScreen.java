f49799963705dd0cd2fbe4a43a1d78f89f5dea64
Pukitis <rovsvg@gmail.com>
2021-08-08 21:03:47 +0300

    Menu update + optimisation update

--- src/main/java/stuntMan/GameScreen.java
+++ src/main/java/stuntMan/GameScreen.java
@@ -221,6 +221,7 @@ public class GameScreen implements ActionListener {
 				i -= coins.get(i).despawn();
 			}
 		}
+		Coin.setAnimationFrame();
 	}
 
 	public static void keyInput(int key, boolean pressed) {
