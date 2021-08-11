a98772a0a2776b6a39076e0472cf813c4e388e7e
Reinis Rozenbahs <reinisrozenbahs@gmail.com>
2021-08-11 10:10:09 +0300

    classpath for the driver

--- src/main/java/stuntMan/startClass.java
+++ src/main/java/stuntMan/startClass.java
@@ -4,7 +4,8 @@ import passwords.Password;
 
 public class startClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] argv) {
+		System.out.println("Begin");
 		Password.getLoginFile();
 		new GameScreen();
 		new MainMenu();
